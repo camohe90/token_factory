175a3ceffd9d13cfbc4b988576cebc9693a9ec48
Camilo Molano <camilo@algorand.foundation>
2024-05-04 15:13:33 -0500

    full testing

--- projects/token_factory/smart_contracts/token_factory/contract.py
+++ projects/token_factory/smart_contracts/token_factory/contract.py
@@ -1,7 +1,42 @@
-from algopy import ARC4Contract, arc4
+from algopy import ARC4Contract, Global, String, Txn, UInt64, arc4, gtxn, itxn
 
 
 class TokenFactory(ARC4Contract):
+    unitary_price: UInt64
+    asset_id: UInt64
+
     @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+    def create_asset(self, asset_name:String, unit_name:String) ->None:
+        self.asset_created = itxn.AssetConfig (
+            asset_name= asset_name,
+            unit_name= unit_name,
+            total= 10_000,
+            decimals= 1,
+            default_frozen=False
+        ).submit().created_asset.id
+
+
+    @arc4.abimethod()
+    def set_price(self, unitary_price:UInt64)->None:
+        assert Txn.sender == Global.creator_address
+        self.unitary_price = unitary_price
+
+    @arc4.abimethod()
+    def sell_asset(self, buyer_txn: gtxn.PaymentTransaction, quantity: UInt64) ->None:
+        assert buyer_txn.sender == Txn.sender
+        assert buyer_txn.receiver == Global.current_application_address
+        assert buyer_txn.amount == self.unitary_price * quantity
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_created,
+            asset_receiver= Txn.sender,
+            asset_amount=quantity
+        ).submit()
+
+
+    @arc4.abimethod
+    def get_asset_id(self)-> UInt64:
+        return self.asset_created
+
+
+
--- projects/token_factory/tests/token_factory_test.py
+++ projects/token_factory/tests/token_factory_test.py
@@ -1,48 +1,144 @@
 import algokit_utils
 import pytest
-from algokit_utils import get_localnet_default_account
-from algokit_utils.config import config
-from algosdk.v2client.algod import AlgodClient
-from algosdk.v2client.indexer import IndexerClient
+from algokit_utils.beta.account_manager import AddressAndSigner
+from algokit_utils.beta.algorand_client import (
+    AlgorandClient,
+    AssetCreateParams,
+    AssetOptInParams,
+    PayParams,
+)
+from algosdk.atomic_transaction_composer import TransactionWithSigner
 
 from smart_contracts.artifacts.token_factory.client import TokenFactoryClient
 
 
 @pytest.fixture(scope="session")
+def algorand() -> AlgorandClient:
+    """Genera un cliente de Algorand para interactuar con la app"""
+    return AlgorandClient.default_local_net()
+
+
+@pytest.fixture(scope="session")
+def dispenser(algorand: AlgorandClient) -> AddressAndSigner:
+    return algorand.account.dispenser()
+
+
+@pytest.fixture(scope="session")
+def creator(algorand: AlgorandClient, dispenser: AddressAndSigner) -> AddressAndSigner:
+    acct = algorand.account.random()
+
+    algorand.send.payment(
+        PayParams(sender=dispenser.address, receiver=acct.address, amount=10_000_000)
+    )
+
+    return acct
+
+
+@pytest.fixture(scope="session")
+def test_asset_id(algorand: AlgorandClient, creator: AddressAndSigner) -> int:
+    sent_txn = algorand.send.asset_create(
+        AssetCreateParams(sender=creator.address, total=10)
+    )
+    return sent_txn["confirmation"]["asset-index"]
+
+
+@pytest.fixture(scope="session")
 def token_factory_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+    algorand: AlgorandClient, creator: AddressAndSigner
 ) -> TokenFactoryClient:
-    config.configure(
-        debug=True,
-        # trace_all=True,
-    )
-
     client = TokenFactoryClient(
-        algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
+        algod_client=algorand.client.algod,
+        sender=creator.address,
+        signer=creator.signer,
     )
 
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
-    )
+    client.create_bare()
+
     return client
 
 
-def test_says_hello(token_factory_client: TokenFactoryClient) -> None:
-    result = token_factory_client.hello(name="World")
+def test_set_price(token_factory_client: TokenFactoryClient):
+    result = token_factory_client.set_price(unitary_price=300_000)
+    assert result.confirmed_round
 
-    assert result.return_value == "Hello, World"
 
 
-def test_simulate_says_hello_with_correct_budget_consumed(
-    token_factory_client: TokenFactoryClient, algod_client: AlgodClient
-) -> None:
-    result = (
-        token_factory_client.compose().hello(name="World").hello(name="Jane").simulate()
-    )
+def test_create_asset(
+        token_factory_client: TokenFactoryClient,
+        creator: AddressAndSigner,
+        algorand: AlgorandClient
+        ):
 
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+        algorand.send.payment(
+            PayParams(
+                    sender = creator.address,
+                    receiver= token_factory_client.app_address,
+                    amount=2_000_000,
+                )
+        )
+
+        assert(
+            algorand.account.get_information(token_factory_client.app_address)["amount"] == 2_000_000
+        )
+
+        sp = algorand.client.algod.suggested_params()
+        sp.fee = 2000
+
+        result = token_factory_client.create_asset(
+            asset_name="New Coin",
+            unit_name="NEW",
+            transaction_parameters=algokit_utils.TransactionParameters(
+                sender=creator.address,
+                signer=creator.signer,
+                suggested_params=sp
+            ),
+        )
+
+        assert result.confirmed_round
+
+        asset_id = token_factory_client.get_asset_id().return_value
+        result = algorand.account.get_information(token_factory_client.app_address)
+
+        assert result["assets"][0]["amount"] == 10000
+        assert asset_id ==  result["assets"][0]["asset-id"]
+
+
+def test_transfer_asset(
+        token_factory_client: TokenFactoryClient,
+        creator: AddressAndSigner,
+        algorand: AlgorandClient
+        ):
+
+        algorand.send.asset_opt_in(
+            AssetOptInParams(
+                sender=creator.address,
+                asset_id= token_factory_client.get_asset_id().return_value
+            )
+        )
+
+        assert algorand.account.get_information(creator.address)["total-assets-opted-in"] == 1
+        asset_id = token_factory_client.get_asset_id().return_value
+
+        buyer_txn = algorand.transactions.payment(
+        PayParams(
+            sender=creator.address,
+            receiver=token_factory_client.app_address,
+            amount=3 * 300_000,
+            extra_fee=1000,
+            )
+        )
+
+        result = token_factory_client.sell_asset(
+            quantity=3,
+            buyer_txn = TransactionWithSigner(txn=buyer_txn, signer=creator.signer),
+            transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[asset_id],
+            sender=creator.address,
+            signer=creator.signer,
+            )
+         )
+
+        assert result.confirmed_round
+
+        result = algorand.account.get_information(creator.address)
+        assert result["assets"][0]["amount"] == 3
